bae1f1ce23f0e6ba2f0e1b2e6255a1cf9dd1d441
Jukka Nikki <jukka.nikki@kolumbus.fi>
2025-11-09 23:36:31 +0100

    added mocking for repo to app test

--- apps/clientapp/test/app/view/app_test.dart
+++ apps/clientapp/test/app/view/app_test.dart
@@ -1,12 +1,22 @@
-//import 'package:clientapp/app/app.dart';
+import 'package:clientapp/app/app.dart';
+import 'package:blog_domain/blog_domain.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:get_it/get_it.dart';
+import 'package:mockito/annotations.dart';
+import 'package:posts/posts.dart';
 
+import 'app_test.mocks.dart';
+
+@GenerateNiceMocks([MockSpec<BlogPostRepository>()])
 void main() {
   group('App', () {
     testWidgets('renders CounterPage', (tester) async {
-      // TODO(jnikki): set up DI / mock posts service before enabling this test
-      //await tester.pumpWidget(App());
-      //expect(find.byType(CounterPage), findsOneWidget);
+
+      final repository =  MockBlogPostRepository();
+      GetIt.instance.registerSingleton<BlogPostRepository>(repository);
+
+      await tester.pumpWidget(App());
+      expect(find.byType(BlogPostsPage), findsOneWidget);
     });
   });
 }
